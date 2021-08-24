edf63c4138663d533a2208f6942e79d96d03cea7
Peter <p.hartzog.koeppen@gmail.com>
2021-08-25 00:17:46 +0200

    fix: removed dist

--- dist/src/helpers/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const render_1 = __importDefault(require("./render"));
-const str = `>greentext\n<redtext`;
-const { rendered } = render_1.default(str);
-console.log('rendered:', rendered);
